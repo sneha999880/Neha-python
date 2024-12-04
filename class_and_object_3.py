800f35029d5023a4bee4abd5347c81098dc8193b
sneha999880 <neharaj96851@gmail.com>
2024-12-04 13:38:57 +0530

    Create class_and_object_3.py

---
+++ class_and_object_3.py
@@ -0,0 +1,9 @@
+class Dogs:
+    def __init__(self,d1,d2,d3):
+        self.d1=d1
+        self.d2=d2
+        self.d3=d3
+    def printdata(self):
+        print(self.d1,self.d2,self.d3)
+b=Dogs("puppy","daisy","bella")
+b.printdata()
