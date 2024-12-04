4305870ccadf9c482b8d240daa59bace5058d052
sneha999880 <neharaj96851@gmail.com>
2024-12-04 13:36:01 +0530

    Create class_and_object.py

---
+++ class_and_object.py
@@ -0,0 +1,9 @@
+class Birds:
+    def __init__(self,b1,b2,b3):
+        self.b1=b1
+        self.b2=b2
+        self.b3=b3
+    def printdata(self):
+        print(self.b1,self.b2,self.b3)
+b=Birds("eagle","Crow","sparrow")
+b.printdata()
