232f6a17403aea708a55eb33e608bad99fec2d88
sneha999880 <neharaj96851@gmail.com>
2024-12-04 13:07:30 +0530

    Create add_list.py

---
+++ add_list.py
@@ -0,0 +1,7 @@
+#write a program to get the input from user for creating two lists and add the elements of one list into the other
+
+size_of_list_1 = int(input())
+list_1 = []
+for i in range(size_of_list_1):
+  list_1.append(int(input()))
+print(list_1)
