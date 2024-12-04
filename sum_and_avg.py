136a7d57df8e23c7a88024de7f79db4ca4a84d72
sneha999880 <neharaj96851@gmail.com>
2024-12-04 11:52:42 +0530

    Create sum_and_avg.py

---
+++ sum_and_avg.py
@@ -0,0 +1,16 @@
+#wap to sum of n numbers
+
+n = int(input("enter n"))
+sum = 0
+for i in range(1,n+1):
+  sum += i
+print (sum)
+
+#take an average of the sum , print sum value and average value
+
+n = int(input("enter n"))
+sum = 0
+for i in range(1,n+1):
+  sum += i
+print (sum)
+print (sum/n)
